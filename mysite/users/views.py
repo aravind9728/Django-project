8368c2cee6fdf637a19993d9aa80303498388da3
aravind9728 <67286139+aravind9728@users.noreply.github.com>
2021-05-02 12:56:04 +0530

    login/fixed/form.save

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -7,6 +7,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'You have create the account {username}! You are now able to login')
             return redirect('login')
